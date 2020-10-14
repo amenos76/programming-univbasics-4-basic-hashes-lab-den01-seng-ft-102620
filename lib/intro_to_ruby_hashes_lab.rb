97885d26372a82a19e4fb960335a9dce381d4020
Augie Menos <augie.menos@gmail.com>
2020-10-14 23:15:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  pets = {
+    :Finn => "Dog",
+    :Tiger => "Cat",
+    :Leonard => "Phat Cat"
+  }
 end
 
 def pioneer
